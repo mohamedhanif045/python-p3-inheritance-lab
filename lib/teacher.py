77eb8d219406e3960a6d6a1b6ff01a3824589cf0
mohamedhanif045 <hanifmohamed045@gmail.com>
2024-08-30 08:18:58 +0300

    Fix Teacher class initialization and add knowledge attribute

--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,21 @@
-#!/usr/bin/env python
-
+import random
 from user import User
 
-import random
-
 class Teacher(User):
+    knowledge = [
+        "str is a data type in Python",
+        "programming is hard, but it's worth it",
+        "JavaScript async web request",
+        "Python function call definition",
+        "object-oriented teacher instance",
+        "programming computers hacking learning terminal",
+        "pipenv install pipenv shell",
+        "pytest -x flag to fail fast",
+    ]
+    
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = Teacher.knowledge  # Initialize with the class-level knowledge list
 
     def teach(self):
-        pass
+        return random.choice(self.knowledge)
